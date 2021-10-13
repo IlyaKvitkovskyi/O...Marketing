5099e54e019af6be0d70af180f7c0dd88b9e0a46
Illia <ilya.kvitkovskyi@gmail.com>
2021-10-13 10:55:17 +0300

    added slider section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(function() {
+$(function () {
   var mixer = mixitup('.portfolio__content');
+
+  $('.slider__blog-inner').slick({
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt="arrow-right"></button>',
+  });
 });
