6af859dfc83f383615d7776e9bcb5393f892617e
Illia <ilya.kvitkovskyi@gmail.com>
2021-10-15 19:58:22 +0300

    added adaptive for header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function () {
-  var mixer = mixitup('.portfolio__content');
 
   $('.slider__blog-inner').slick({
     dots: true,
@@ -8,4 +7,10 @@ $(function () {
     nextArrow:
       '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt="arrow-right"></button>',
   });
+
+  $('.menu__btn').on('click', function(){
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
+
+  var mixer = mixitup('.portfolio__content');
 });
