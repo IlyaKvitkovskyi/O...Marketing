2998e2906d0486f61db69226cd98ac5b59ee9edc
Illia <ilya.kvitkovskyi@gmail.com>
2021-10-25 16:43:36 +0300

    added sticky menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(function () {
+  $('.menu a, .go-top').on('click', function (event) {
+    event.preventDefault();
+
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
 
   $('.slider__blog-inner').slick({
     dots: true,
@@ -11,12 +19,12 @@ $(function () {
         breakpoint: 769,
         settings: {
           arrows: false,
-        }
+        },
       },
-    ]
+    ],
   });
 
-  $('.menu__btn').on('click', function(){
+  $('.menu__btn, .menu a').on('click', function () {
     $('.header__top-inner').toggleClass('header__top-inner--active');
   });
 
